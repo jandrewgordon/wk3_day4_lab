72ef8c492cef9bcd39ba74d6e1df969c30abba21
jandrewgordon <jandrewgordon@gmail.com>
2021-05-20 11:23:17 +0100

    push for David

---
+++ models/event.py
@@ -0,0 +1,8 @@
+class Event:
+
+    def __init___(self, date, name, guests, room, description):
+        self.name = name
+        self.date = date
+        self.guests = guests
+        self.room = room
+        self.description = description
